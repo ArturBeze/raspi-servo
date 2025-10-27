8f9517b337838d79ac631d92c5d2397bacdb7ec6
Artur Beze <155668924+ArturBeze@users.noreply.github.com>
2025-10-27 17:31:06 +0300

    Update preview_depth.py

--- preview_depth.py
+++ preview_depth.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import ArducamDepthCamera as ac
 
+import cv2.ximgproc as xip
+
 # MAX_DISTANCE value modifiable  is 2000 or 4000
 MAX_DISTANCE=4000
 
@@ -35,16 +37,11 @@ confidence_value = 30
 selectRect, followRect = UserRect(), UserRect()
 
 
-def getPreviewRGB(preview: np.ndarray, confidence: np.ndarray) -> np.ndarray:
-    preview = np.nan_to_num(preview)
-    preview[confidence < confidence_value] = (0, 0, 0)
-    return preview
-
-
-def getPreviewBW(preview: np.ndarray, confidence: np.ndarray) -> np.ndarray:
-    preview = np.nan_to_num(preview)
-    preview[confidence < confidence_value] = 0
-    return preview
+def getPreview(preview: np.ndarray, confidence: np.ndarray) -> np.ndarray:
+    mask = confidence > confidence_value
+    depth_masked = preview.copy()
+    depth_masked[~mask] = 0
+    return depth_masked
 
 
 def on_mouse(event, x, y, flags, param):
@@ -106,15 +103,24 @@ def main():
 
     r = cam.getControl(ac.Control.RANGE)
     print(f"r = {r}, 255 / r = {255.0 / r}")
-
+    
     info = cam.getCameraInfo()
     print(f"Camera resolution: {info.width}x{info.height}")
+
+    cv2.namedWindow("result image", cv2.WINDOW_AUTOSIZE)
+    cv2.setMouseCallback("result image", on_mouse)
+
+    cv2.namedWindow("preview confidence", cv2.WINDOW_AUTOSIZE)
+    cv2.setMouseCallback("preview confidence", on_mouse)
 
     cv2.namedWindow("rgb", cv2.WINDOW_AUTOSIZE)
     cv2.setMouseCallback("rgb", on_mouse)
 
-    if info.device_type == ac.DeviceType.VGA:
-        # Only VGA support confidence
+    cv2.namedWindow("bw", cv2.WINDOW_AUTOSIZE)
+    cv2.setMouseCallback("bw", on_mouse)
+
+    if info.device_type == ac.DeviceType.HQVGA:
+        # Only HQVGA support confidence
         cv2.createTrackbar(
             "confidence", "rgb", confidence_value, 255, on_confidence_changed
         )
@@ -122,36 +128,37 @@ def main():
     while True:
         frame = cam.requestFrame(2000)
         if frame is not None and isinstance(frame, ac.DepthData):
-            depth_buf = frame.depth_data            
-            confidence_buf = frame.confidence_data
-            amplitude_buf = frame.amplitude_data
-            
-            height, width = frame.depth_data.shape
+            depth_buf = frame.depth_data # map of distances
+            confidence_buf = frame.confidence_data # how reliable each depth measurement
+            #amplitude_buf = frame.amplitude_data # strength of the returned light signal
 
             result_image = (depth_buf * (255.0 / r)).astype(np.uint8)
+
             RGB_image = cv2.applyColorMap(result_image, cv2.COLORMAP_RAINBOW)
-            RGB_image = getPreviewRGB(RGB_image, confidence_buf)
-            BW_image = getPreviewBW(result_image, confidence_buf)
-            
-            #print(f"confidence_pre_buf[{int(height/2)},{int(width/2)}] = {confidence_buf[int(height/2)][int(width/2)]}")
+
+            BW_image = getPreview(result_image, confidence_buf)
+            RGB_image = getPreview(RGB_image, confidence_buf)
 
             cv2.normalize(confidence_buf, confidence_buf, 1, 0, cv2.NORM_MINMAX)
 
-            cv2.imshow("preview_confidence", confidence_buf)
+            BW_image = cv2.medianBlur(BW_image, ksize=5)
+            BW_image = cv2.GaussianBlur(BW_image, (5, 5), 0)
+            
+            RGB_image = cv2.bilateralFilter(RGB_image, d=9, sigmaColor=50, sigmaSpace=9)
+
+            #cv2.imshow("result image", result_image)
+            #cv2.imshow("preview confidence", confidence_buf)
 
             cv2.rectangle(RGB_image, followRect.rect, white_color, 1)
             if not selectRect.empty:
                 cv2.rectangle(RGB_image, selectRect.rect, black_color, 2)
                 print("select Rect distance:", np.mean(depth_buf[selectRect.slice]))
-            
-            #print(f"\ndepth_buf[{int(height/2)},{int(width/2)}] = {depth_buf[int(height/2)][int(width/2)]}")
-            #print(f"confidence_buf[{int(height/2)},{int(width/2)}] = {confidence_buf[int(height/2)][int(width/2)]}")
-            #print(f"amplitude_buf[{int(height/2)},{int(width/2)}] = {amplitude_buf[int(height/2)][int(width/2)]}\n")
-            
+
             cv2.imshow("rgb", RGB_image)
             cv2.imshow("bw", BW_image)
+
             cam.releaseFrame(frame)
-            
+
         key = cv2.waitKey(1)
         if key == ord("q"):
             break
